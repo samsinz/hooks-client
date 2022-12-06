cce454155185fe9ba609e5789d336b08ccb47e71
Sam Sinz <sam@sinz.fr>
2022-12-06 18:07:48 +0100

    hooks

---
+++ src/components/charts/Tags.jsx
@@ -0,0 +1,31 @@
+import "../../styles/Hooks/hooksComponents.css";
+
+const Tags = ({ hook }) => {
+  return (
+    <div className="Tags">
+      <div>
+        <div className="tags-container">
+          <span className={hook.type === 'Kissing'? "selected" : "unselected"}>Kissing</span>
+          <span className={hook.type === 'Foreplay'? "selected" : "unselected"}>Foreplay</span>
+          <span className={hook.type === 'Sex'? "selected" : "unselected"}>Sex</span>
+        </div>
+        <div className="tags-container2">
+          <span className={hook.protection? "selected" : "unselected"}>Protected</span>
+          <span className={hook.orgasm? "selected" : "unselected"}>Orgasm</span>
+        </div>
+      </div>
+      <div className="gauge-container">
+        <div className="gauge">
+          <div className="level" style={{ width: `${hook.duration * 50}%` }}></div>
+        </div>
+        <div className="legend">
+          <h3>Too short</h3>
+          <h3>Perfect</h3>
+          <h3>Too long</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tags;
